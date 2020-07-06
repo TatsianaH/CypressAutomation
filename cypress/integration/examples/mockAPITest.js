f1a4925e4dc0a30d20b8596401acad3799d33607
TatsianaH <ourflsun@gmail.com>
2020-07-06 18:53:15 -0400

    add API mock test

---
+++ cypress/integration/examples/mockAPITest.js
@@ -0,0 +1,21 @@
+/// <reference types="Cypress" />
+
+describe('API mock test', () => {
+  it('should return an error message', function () {
+    cy.visit('https://example.cypress.io/commands/network-requests');
+    cy.server();
+    cy.route({
+      method: 'PUT',
+      url: '/comments/*',
+      status: 404,
+      response: {
+        error: 'No comment found',
+      },
+      delay: 1000,
+    }).as('updateComment');
+
+    cy.get('.network-put').click();
+    cy.wait('@updateComment');
+    cy.get('.network-put-comment').should('contain', 'No comment found');
+  });
+});
